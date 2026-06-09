8d79f14c13d27ac96cdae3f0fd466cf2de32f930
Conny Ohult Andersen <cohult@cisco.com>
2026-06-09 12:38:26 +0200

    Declare DNS names and IP addresses in the Subject Alternative Name (SAN) extension

--- raft-l3bgp/raft-etc/gen_tls_certs.sh
+++ raft-l3bgp/raft-etc/gen_tls_certs.sh
@@ -10,6 +10,58 @@ NC='\033[0m' # No Color
 scrptname=${0##*/}
 NODES=( ${NODE1} ${NODE2} ${NODE3} )
 
+subject_alt_name_entry()
+{
+    case "$1" in
+        ""|*_* )
+            return
+            ;;
+        *:* )
+            printf "IP:%s" "$1"
+            ;;
+        *[!0-9.]* )
+            printf "DNS:%s" "$1"
+            ;;
+        *.* )
+            printf "IP:%s" "$1"
+            ;;
+        * )
+            printf "DNS:%s" "$1"
+            ;;
+    esac
+}
+
+create_subject_alt_name()
+{
+    CERT_NAME="$1"
+    shift
+
+    SAN=""
+    for HOST in "$CERT_NAME" "$@" ; do
+        ENTRY=$(subject_alt_name_entry "$HOST")
+        if [ -n "$ENTRY" ]; then
+            if [ -n "$SAN" ]; then
+                SAN="${SAN}, ${ENTRY}"
+            else
+                SAN="$ENTRY"
+            fi
+        fi
+    done
+
+    if [ -n "$SAN" ]; then
+        printf "subjectAltName=%s\n" "$SAN" > "/ssl/csr/$CERT_NAME.san"
+    fi
+}
+
+node_ip()
+{
+    case "$1" in
+        "$NODE1") printf "%s" "${NODE1_IP:-}" ;;
+        "$NODE2") printf "%s" "${NODE2_IP:-}" ;;
+        "$NODE3") printf "%s" "${NODE3_IP:-}" ;;
+    esac
+}
+
 # certificate authority certs and keys
 generate_self_signed_ca()
 {
@@ -27,6 +79,9 @@ generate_self_signed_ca()
 
 generate_host_certs()
 {
+    NODE_IP=$(node_ip "$1")
+    create_subject_alt_name "$1" "$NODE_IP"
+
     # umask removes group/other read/write access from private key
     ( set -x; umask 077 && \
           openssl req -newkey ec -pkeyopt ec_paramgen_curve:secp384r1 \
@@ -36,6 +91,7 @@ generate_host_certs()
                           -in "/ssl/csr/$1.csr" \
                           -CA "/ssl/certs/ca.crt" \
                           -CAkey "/ssl/private/ca.key" \
+                          -extfile "/ssl/csr/$1.san" \
                           -days 3652 -out "/$1/etc/dist/ssl/certs/$1.crt" )
 
     if [ $? -ne 0 ]; then
